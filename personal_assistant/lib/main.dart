bda949ed0f70be5e216889adf16611999e439b42
Eddydev-ALU <e.irasetsa@alustudent.com>
2026-02-04 19:08:29 +0200

    added the attendance tracking screen

--- personal_assistant/lib/main.dart
+++ personal_assistant/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'navigation_menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,17 +7,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Scaffold(),
-      debugShowCheckedModeBanner: false
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const NavigationMenu(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
