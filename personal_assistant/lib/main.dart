cdfe84aae1a89fad87c22177d218095c9939a66f
Yvette334 <y.muhoracye@alustudent.com>
2026-02-05 14:35:52 +0200

    home

--- personal_assistant/lib/main.dart
+++ personal_assistant/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_assistant/screens/dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,8 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomeDashboard(),
     );
   }
 }
