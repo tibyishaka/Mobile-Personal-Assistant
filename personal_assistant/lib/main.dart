907cfa27b4762c903b3fca8071f500f4c415a1e5
tibyishaka <t.ibyishaka@alustudent.com>
2026-02-06 07:18:56 +0200

    changed Material app tittle

--- personal_assistant/lib/main.dart
+++ personal_assistant/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => ScheduleProvider()),
       ],
       child: MaterialApp(
-        title: 'Personal Assistant',
+        title: 'Student Academic Assistant',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
