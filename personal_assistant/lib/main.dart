ed399eaecad6d39418c7fdf465df592dedd5cb61
KABASINGAarsene <a.kabasinga@alustudent.com>
2026-02-06 16:37:58 +0200

    merge: resolve conflict with main branch

--- personal_assistant/lib/main.dart
+++ personal_assistant/lib/main.dart
@@ -32,19 +32,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
 
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('ALU Student Assistant')),
-      body: ScheduleScreen(),
-    );
-  }
-}
